888c3fb16eb281f009b34303c2a3d2ec0a4ab306
NjengaC <victorcyrus01@gmail.com>
2025-02-14 16:08:01 +0300

    Feat: Conditionally render stage2

--- api/router.py
+++ api/router.py
@@ -7,7 +7,7 @@ api_router.include_router(books.router, prefix="/books", tags=["books"])
 
 # Only include /stage2 if ENABLE_STAGE2 is True.
 from core.config import settings
-if settings.ENABLE_STAGE2:
+if settings.ENABLE_STAGE2 == True:
     from api.routes import stage2
     # This makes the endpoint available at /stage2
     api_router.include_router(stage2.router, prefix="/stage2", tags=["stage2"])
